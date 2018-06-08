3a3228226bfda42202a15a97885a2a0ff4434984
Emily Carey <emilyrosinacarey@gmail.com>
2018-06-08 20:50:25 +0100

    Add getters to help filter recipes, filters and jobs

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,3 +1,20 @@
 export default {
-
+  matchedRecipes (state) {
+    return state.recipes[state.searchTerm]
+  },
+  filtersAreActive (state) {
+    return Object.values(state.filters).includes(true)
+  },
+  matchedJobs (state, getters) {
+    if (!getters.matchedRecipes) return null
+    else {
+      let jobs = []
+      getters.matchedRecipes.forEach(recipe => {
+        if (!jobs.includes(recipe.job_code)) {
+          jobs.push(recipe.job_code)
+        }
+      })
+      return jobs
+    }
+  }
 }
