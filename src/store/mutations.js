b47cdfa861545bf6c0e55f1dc61de300c1615333
Emily Carey <emilyrosinacarey@gmail.com>
2018-06-08 20:51:06 +0100

    Add mutations to set state props (recipes, search term, filter)

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,3 +1,13 @@
-export default {
+import Vue from 'vue'
 
+export default {
+  SET_RECIPES (state, { searchTerm, recipes }) {
+    Vue.set(state.recipes, searchTerm, recipes)
+  },
+  SET_SEARCHTERM (state, searchTerm) {
+    state.searchTerm = searchTerm
+  },
+  TOGGLE_FILTER (state, jobCode) {
+    state.filters[jobCode] = !state.filters[jobCode]
+  }
 }
